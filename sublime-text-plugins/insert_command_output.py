a307016f7f0704cb9461b8f9168c2b39b4ca3a12
Jim Morris <morris@wolfman.com>
2025-05-28 17:29:09 +0100

    update the insert command plugin

--- sublime-text-plugins/insert_command_output.py
+++ sublime-text-plugins/insert_command_output.py
@@ -15,15 +15,13 @@ class InsertCommandOutputCommand(sublime_plugin.TextCommand):
         if 'cmd' in args:
             command = args['cmd']
 
-            # Replace this with your shell command
-            # command = 'lookup-svd -p SPI0 -r SSpSR -a'
-
             try:
                 # Run the command and capture output
                 output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
                 output_text = output.decode("utf-8")
             except subprocess.CalledProcessError as e:
-                output_text = f"Error:\n{e.output.decode('utf-8')}"
+                sublime.status_message(f"Error: {e.output.decode('utf-8')}")
+                return
 
             # Insert output at the first cursor/selection
             for region in self.view.sel():
@@ -37,4 +35,33 @@ class InsertCommandOutputCommand(sublime_plugin.TextCommand):
     def on_done(self, command_str):
         if not command_str.strip():
             return
+        if command_str.startswith('/'):
+            pp = self.get_project_path()
+            if pp:
+                command_str = pp + command_str
+
         self.view.run_command("insert_command_output", {"cmd": command_str})
+
+    def get_project_path(self):
+        # Get the current file path
+        file_path = self.view.file_name()
+        if not file_path:
+            sublime.status_message("No file to copy path from.")
+            return None
+
+        else:
+            # Find the project root
+            window = self.view.window()
+            folders = window.folders()
+            project_root = None
+            for folder in folders:
+                if file_path.startswith(folder):
+                    project_root = folder
+                    break
+
+        if not project_root:
+            sublime.status_message("File is not in any of the loaded projects.")
+            return None
+
+        # print(f"project root: {project_root}")
+        return project_root
